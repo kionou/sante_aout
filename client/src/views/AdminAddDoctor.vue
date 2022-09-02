cb2f008cda2a84755617a38e95d57ab5680866c4
kionou <kionoumamadou.00@gmail.com>
2022-09-02 17:10:24 +0000

    fin

--- client/src/views/AdminAddDoctor.vue
+++ client/src/views/AdminAddDoctor.vue
@@ -1,134 +1,315 @@
 <template>
-  <div>
-    <section class="vh-100" style="background-color: #2779e2;">
-        <div class="container h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-xl-9">
+  <div class="container">
+      <div class="content">
       
-              <h1 class="text-white mb-4">Enregistrer un Professionnel de sante</h1>
-             
-                <form action="/insertdoctor" method="post">
-                  <div class="card" style="border-radius: 15px;">
-                    <div class="card-body">
-          
-                      <div class="row align-items-center pt-4 pb-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0">Nom</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text"  name="nom" class="form-control form-control-lg"  />
-          
-                        </div>
-                      </div>
-                      <div class="row align-items-center pt-4 pb-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0">Prenom</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text" name="prenom" class="form-control form-control-lg" />
-          
-                        </div>
-                      </div>
-          
-                      <hr class="mx-n3">
-          
-                      <div class="row align-items-center py-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0"> Addresse Email</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="email" name="email" class="form-control form-control-lg" placeholder="example@example.com"  />
-          
-                        </div>
-                      </div>
-                      <div class="row align-items-center py-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0"> Numero</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text" name="numero" class="form-control form-control-lg"  />
-          
-                        </div>
-                      </div>
-          
-                      <hr class="mx-n3">
-          
-                      <div class="row align-items-center py-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0">Specialité</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text" name="specialite" class="form-control form-control-lg"  />
-          
-                        </div>
-                      </div>
-                      <div class="row align-items-center py-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0">Centre</h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text" name="centre" class="form-control form-control-lg"  />
-          
-                        </div>
-                      </div>
-          
-                      <hr class="mx-n3">
-          
-                      <div class="row align-items-center py-3">
-                        <div class="col-md-3 ps-5">
-          
-                          <h6 class="mb-0">Mot de passe </h6>
-          
-                        </div>
-                        <div class="col-md-9 pe-5">
-          
-                          <input type="text" name="password" class="form-control form-control-lg"  />
-          
-                        </div>
-          
-                      <hr class="mx-n3">
-          
-                      <div class="px-5 py-4">
-                        <button  class="btn btn-primary btn-lg">Valider</button>
-                      </div>
-          
-                    </div>
-                  </div>
-                  </div>
-                </form>
+       <div class="carnet">
+           <h2>Ajouter un professionnel de santé</h2>
+           <form action="">
+        
+               <div class="form-content">
+                   <label for="">Nom</label>
+                   <input type="text" v-model="nom" >
+               </div>
+                <div class="form-content">
+                   <label for="">Prenom</label>
+                   <input type="text" v-model="prenom" >
+               </div>
+                <div class="form-content">
+                   <label for="">Adresse Email</label>
+                   <input type="text" v-model="email" >
+               </div>
+                <div class="form-content">
+                   <label for="">Numero</label>
+                   <input type="text" v-model="numero" >
+               </div>
+                <div class="form-content">
+                   <label for=""></label>
+                   <input type="text"  v-model="date_naissance">
+               </div>
+               <div class="form-content">
+                   <label for="">Numero</label>
+                   <input type="text" v-model="numero" >
+               </div>
+                <div class="form-content">
+                   <label for="">Date de naissance</label>
+                   <input type="text"  v-model="date_naissance">
+               </div>
+              
+               <button @click.prevent="submit">Ajouter</button>
+              
+           </form>
+        {{users}}
+        
       
-            </div>
-          </div>
-        </div>
-      </section>
-
-  </div>
+       </div>
+      </div>
+   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+     name:"AdminAddDoctor",
+     data() {
+       return {
+              //  nom:this.users.nom,
+              //  prenom:this.users.prenom,
+              //  email:this.users.email,
+              //  numero:this.users.numero,
+              //  date_naissance:this.users.date_naissance,
+                   
+       }
+     },
+   methods:{
+       carnet(){
+           this.$router.push({path:"/carnet"})
+       },
+       compte(){
+           this.$router.push({path:"/compte"})
+       },
+       maladie(){
+           this.$router.push({path:"/maladie"})
+       },
+
+         submit(){
+           
+           // this.v$.$validate()
+           // this.v$.$touch()
+           // if (this.v$.$errors.length == 0 ) {
+               // this.revele = !this.revele
+          //  let   DataUser={
+          //          nom:this.nom,
+          //          prenom:this.prenom,
+          //          email:this.email,
+          //          numero:this.numero,
+          //          date_naissance:this.date_naissance,
+                  
+          //      }
+          //      console.log("dsqf",DataUser);
+                  axios.post('https://sante-kionou.herokuapp.com/users/updateuser',DataUser)
+                 .then((response) => {
+                   console.log('message',response.data)
+                   
+
+                   if (response.data.data) {
+                      
+                       this.$router.push({path:'/listevaccin'})
+   
+                   }
+                  
+                 
+                 })
+               
+           }
+       // }
+   }
 
 }
 </script>
 
-<style>
+<style scoped lang="css">
+.container {
+    background-color: #f5f5f5;
+   display: flex;
+   flex-direction: column;
+   /* justify-content: center; */
+   align-items: center;
+   
+   width: 100%;
+   height: calc(100vh - 75px);
+   padding: 10px;
+}
+
+.content {
+   width: 80%;
+   height: 100%;
+   /* border: 1px solid black; */
+   padding: 10px;
+   display: flex;
+   flex-direction: column;
+   background-color: #fff;
+   border-radius: 8px;
+}
+
+.header{
+   border: 1px solid #B3B3B3;
+   padding: 10px;
+   border-radius: 10px;
+}
+.header ul{
+   display: flex;
+   justify-content: space-evenly;
+   /* border: 1px solid blue; */
+}
+.header ul li {
+   list-style: none;
+   /* border: 1px solid red; */
+   padding: 10px 18px;   
+}
+.header ul li {
+   text-decoration: none;
+   padding: 10px 18px;
+   border-radius: 30px;
+   border: 1px solid #B3B3B3;
+   color: white;
+   background-color: rgb(15, 142, 240); 
+}
+.header ul li:hover{
+   color:rgb(15, 142, 240) ;
+   background-color: #fff;
+   border: 1px solid rgb(15,142,240);
+   cursor: pointer;
+}
+
+
+.carnet {
+   border: solid 1px #B3B3B3;
+   height: 100%;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-evenly;
+   align-items: center;
+   margin-top: 20px;
+   border-bottom-left-radius: 10px;
+   border-bottom-right-radius: 10px;
+}
+h3{
+   font-size: 25px;
+   padding: 15px 0;
+}
+form{
+   border: 1px solid #d8d8d8;
+   display: flex;
+   flex-direction: column;
+   padding: 10px;
+   max-width: 400px;
+   width: 98%;
+   align-items: center;
+   height: 72vh;
+   justify-content: center;
+   border-radius: 8px;
+    box-shadow: 0 1px 5px rgb(0 0 0 / 20%);
+}
+
+
+.form-content  {
+   /* border: 1px solid blue; */
+   display: flex;
+   flex-direction: column;
+   margin-bottom: 15px;
+   width: 100%;
+   align-items: center;
+}
+label{
+   font-size: 18px;
+}
+input{
+   max-width: 400px;
+   width: 98%;
+   height: 2.5rem;
+   border-radius: 8px;
+   /* outline: none; */
+   padding: 8px;
+   font-size: 18px;
+   border: 1px solid;
+}
+
+button {
+   width: 9rem;
+   height: 3rem;
+   margin-top: 25px;
+   text-align: center;
+   border: none;
+   background-color: rgb(15, 142, 240);
+   color: white;
+   border-radius: 5px;
+   font-size: 23px;
+    font-family: 'Kaushan Script', cursive;
+}
+
+button:hover {
+   background-color: white;
+   color: rgb(15, 142, 240);
+   border: 1px solid rgb(15, 142, 240);
+   cursor: pointer;
+}
+
+@media (max-width: 940px) {
+   .content {
+       width: 70%;
+   }
+
+   .carnet .text {
+       font-size: 30px;
+   }
+
+}
+
+@media (max-width: 700px) {
+   .content {
+       width: 100%;
+       flex-direction: row;
+   }
+
+   .header ul {
+       flex-direction: column;
+       justify-content: center;
+   }
+
+   .carnet {
+       width: 100%;
+       height: auto;
+       margin-top: 0;
+       justify-content: center;
+   }
+
+   .header {
+       display: flex;
+   }
+
+   .header ul li {
+       height: 3rem;
+       text-align: center;
+       display: flex;
+       padding: 0;
+       margin-top: 10px;
+       background-color: rgb(15, 142, 240);
+   }
+
+   .header ul li:nth-child(odd) {
+       padding-top: 10px;
+   }
+
+   .header ul li a {
+       height: 100%;
+       width: 100%;
+       padding: 0;
+       border-radius: 0px;
+       background-color: transparent;
+       border: none;
+   }
+
+   .header ul li a:hover {
+       border: none;
+   }
+
+   .header ul li:hover {
+       background-color: #fff;
+       color: rgb(15, 142, 240);
+   }
+
+   .carnet .text {
+       font-size: 20px;
+   }
+
+}
+
+@media (max-width: 378px) {
+
+   .carnet .text {
+       font-size: 10px;
+   }
+
+}
 
 </style>
